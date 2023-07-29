23be4c6120eaf1031b1e81c0cdab7301b187f401
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-29 17:24:02 -0500

    Update main.js

--- backend/admin/main.js
+++ backend/admin/main.js
@@ -86,4 +86,84 @@ function displayExistingProducts() {
   });
 }
 
-// Event listener
+// Main JavaScript (main.js)
+
+// Function to update an existing product in the Firestore database
+function updateProductInDatabase(productId, updatedProductData) {
+  // Assuming you have already initialized Firebase and Firestore
+  const productsCollection = firebase.firestore().collection('products');
+
+  // Update the product in Firestore
+  productsCollection.doc(productId).update(updatedProductData)
+    .then(() => {
+      console.log('Product updated successfully.');
+    })
+    .catch((error) => {
+      console.error('Error updating product:', error);
+    });
+}
+
+// Function to delete a product from the Firestore database
+function deleteProductFromDatabase(productId) {
+  // Assuming you have already initialized Firebase and Firestore
+  const productsCollection = firebase.firestore().collection('products');
+
+  // Delete the product from Firestore
+  productsCollection.doc(productId).delete()
+    .then(() => {
+      console.log('Product deleted successfully.');
+    })
+    .catch((error) => {
+      console.error('Error deleting product:', error);
+    });
+}
+
+// Event listener for submitting the Add Product form
+document.getElementById('add-product-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get form values
+  const productName = document.getElementById('product-name').value;
+  const productDescription = document.getElementById('product-description').value;
+  const productPrice = parseFloat(document.getElementById('product-price').value);
+  const productCategory = document.getElementById('product-category').value;
+
+  // Call the function to add the new product to the database
+  addNewProductToDatabase(productName, productDescription, productPrice, productCategory);
+});
+
+// Event listener for dynamically added Edit and Delete buttons
+document.getElementById('existing-products').addEventListener('click', function(event) {
+  if (event.target.classList.contains('edit-product-btn')) {
+    // Get the product ID associated with the Edit button
+    const productId = event.target.dataset.productId;
+
+    // Fetch the existing product data from Firestore
+    // (Optional: You may want to display the existing data in the Add Product form for editing)
+    // You can use a separate function to fetch the data based on productId.
+
+    // Example:
+    // productsCollection.doc(productId).get()
+    //   .then((doc) => {
+    //     const productData = doc.data();
+    //     // Populate the form with existing data for editing
+    //     document.getElementById('product-name').value = productData.name;
+    //     document.getElementById('product-description').value = productData.description;
+    //     document.getElementById('product-price').value = productData.price;
+    //     document.getElementById('product-category').value = productData.category;
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching product data for editing:', error);
+    //   });
+
+  } else if (event.target.classList.contains('delete-product-btn')) {
+    // Get the product ID associated with the Delete button
+    const productId = event.target.dataset.productId;
+
+    // Call the function to delete the product from the database
+    deleteProductFromDatabase(productId);
+  }
+});
+
+// Call the function to display existing products on page load
+displayExistingProducts();
