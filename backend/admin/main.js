67203706ca3d337d7b8bf64817846b13a84162b9
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-29 17:46:09 -0500

    Update main.js

--- backend/admin/main.js
+++ backend/admin/main.js
@@ -1,201 +1,110 @@
 // Main JavaScript (main.js)
 
-// Function to add new product to the Firestore database
-function addNewProductToDatabase(name, description, price, category) {
-  // Assuming you have already initialized Firebase and Firestore
-  const productsCollection = firebase.firestore().collection('products');
+// Function to upload an image to Firebase Storage
+function uploadImageToStorage(file) {
+  // Assuming you have already initialized Firebase and Firebase Storage
+  const storageRef = firebase.storage().ref();
 
-  // Add the product to Firestore
-  productsCollection.add({
-    name: name,
-    description: description,
-    price: price,
-    category: category
-  })
-  .then((docRef) => {
-    console.log('Product added with ID:', docRef.id);
-    // Clear the form fields after successful product addition
-    document.getElementById('product-name').value = '';
-    document.getElementById('product-description').value = '';
-    document.getElementById('product-price').value = '';
-    document.getElementById('product-category').value = '';
-  })
-  .catch((error) => {
-    console.error('Error adding product:', error);
-  });
-}
+  // Generate a unique ID for the image (you can use the filename or any other method)
+  const imageId = new Date().getTime().toString();
 
-// Function to fetch and display existing products from Firestore
-function displayExistingProducts() {
-  const existingProductsContainer = document.getElementById('existing-products');
-
-  // Clear the container before adding products (to avoid duplicates)
-  existingProductsContainer.innerHTML = '';
-
-  // Assuming you have already initialized Firebase and Firestore
-  const productsCollection = firebase.firestore().collection('products');
-
-  // Fetch products from Firestore and display them
-  productsCollection.get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      const productData = doc.data();
-
-      // Create a div to display each product
-      const productDiv = document.createElement('div');
-      productDiv.classList.add('product');
-
-      // Add product details to the div
-      const productNameHeading = document.createElement('h3');
-      productNameHeading.textContent = productData.name;
-
-      const productDescriptionParagraph = document.createElement('p');
-      productDescriptionParagraph.textContent = productData.description;
-
-      const productPriceParagraph = document.createElement('p');
-      productPriceParagraph.textContent = `Price: $${productData.price.toFixed(2)}`;
-
-      const productCategoryParagraph = document.createElement('p');
-      productCategoryParagraph.textContent = `Category: ${productData.category}`;
-
-      // Add Edit and Delete buttons to each product
-      const editButton = document.createElement('button');
-      editButton.classList.add('edit-product-btn');
-      editButton.textContent = 'Edit';
-      editButton.dataset.productId = doc.id;
-
-      const deleteButton = document.createElement('button');
-      deleteButton.classList.add('delete-product-btn');
-      deleteButton.textContent = 'Delete';
-      deleteButton.dataset.productId = doc.id;
-
-      // Append elements to the product div
-      productDiv.appendChild(productNameHeading);
-      productDiv.appendChild(productDescriptionParagraph);
-      productDiv.appendChild(productPriceParagraph);
-      productDiv.appendChild(productCategoryParagraph);
-      productDiv.appendChild(editButton);
-      productDiv.appendChild(deleteButton);
-
-      // Append the product div to the container
-      existingProductsContainer.appendChild(productDiv);
-    });
-  })
-  .catch((error) => {
-    console.error('Error fetching products:', error);
-  });
-}
-
-// Main JavaScript (main.js)
-
-// Function to update an existing product in the Firestore database
-function updateProductInDatabase(productId, updatedProductData) {
-  // Assuming you have already initialized Firebase and Firestore
-  const productsCollection = firebase.firestore().collection('products');
-
-  // Update the product in Firestore
-  productsCollection.doc(productId).update(updatedProductData)
-    .then(() => {
-      console.log('Product updated successfully.');
+  // Upload the image to Firebase Storage with the unique ID
+  const imageRef = storageRef.child(`gallery_images/${imageId}`);
+  imageRef.put(file)
+    .then((snapshot) => {
+      console.log('Image uploaded successfully.');
+      // Call the function to update the image gallery in the UI
+      displayImageGallery();
     })
     .catch((error) => {
-      console.error('Error updating product:', error);
-    });
-}
-
-// Function to delete a product from the Firestore database
-function deleteProductFromDatabase(productId) {
-  // Assuming you have already initialized Firebase and Firestore
-  const productsCollection = firebase.firestore().collection('products');
-
-  // Delete the product from Firestore
-  productsCollection.doc(productId).delete()
-    .then(() => {
-      console.log('Product deleted successfully.');
-    })
-    .catch((error) => {
-      console.error('Error deleting product:', error);
-    });
-}
-
-// Event listener for submitting the Add Product form
-document.getElementById('add-product-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  // Get form values
-  const productName = document.getElementById('product-name').value;
-  const productDescription = document.getElementById('product-description').value;
-  const productPrice = parseFloat(document.getElementById('product-price').value);
-  const productCategory = document.getElementById('product-category').value;
-
-  // Call the function to add the new product to the database
-  addNewProductToDatabase(productName, productDescription, productPrice, productCategory);
-});
-
-// Event listener for dynamically added Edit and Delete buttons
-document.getElementById('existing-products').addEventListener('click', function(event) {
-  if (event.target.classList.contains('edit-product-btn')) {
-    // Get the product ID associated with the Edit button
-    const productId = event.target.dataset.productId;
-
-    // Fetch the existing product data from Firestore
-    // (Optional: You may want to display the existing data in the Add Product form for editing)
-    // You can use a separate function to fetch the data based on productId.
-
-    // Example:
-    // productsCollection.doc(productId).get()
-    //   .then((doc) => {
-    //     const productData = doc.data();
-    //     // Populate the form with existing data for editing
-    //     document.getElementById('product-name').value = productData.name;
-    //     document.getElementById('product-description').value = productData.description;
-    //     document.getElementById('product-price').value = productData.price;
-    //     document.getElementById('product-category').value = productData.category;
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error fetching product data for editing:', error);
-    //   });
-
-  } else if (event.target.classList.contains('delete-product-btn')) {
-    // Get the product ID associated with the Delete button
-    const productId = event.target.dataset.productId;
-
-    // Call the function to delete the product from the database
-    deleteProductFromDatabase(productId);
-  }
-});
-
-// Call the function to display existing products on page load
-displayExistingProducts();
-
-
-
-
-// Main JavaScript (main.js)
-
-// Function to authenticate admin users with email and password
-function loginAdminUser(email, password) {
-  // Assuming you have already initialized Firebase and Firebase Auth
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-      console.log('Admin user logged in successfully.');
-      // Redirect to the admin dashboard page or update UI as needed
-    })
-    .catch((error) => {
-      console.error('Error logging in:', error);
+      console.error('Error uploading image:', error);
       // Display error message to the user, e.g., in the error bar
     });
 }
 
-// Event listener for submitting the Login form
-document.getElementById('login-form').addEventListener('submit', function(event) {
+// Function to delete an image from Firebase Storage
+function deleteImageFromStorage(imageId) {
+  // Assuming you have already initialized Firebase and Firebase Storage
+  const storageRef = firebase.storage().ref();
+
+  // Delete the image from Firebase Storage
+  const imageRef = storageRef.child(`gallery_images/${imageId}`);
+  imageRef.delete()
+    .then(() => {
+      console.log('Image deleted successfully.');
+      // Call the function to update the image gallery in the UI
+      displayImageGallery();
+    })
+    .catch((error) => {
+      console.error('Error deleting image:', error);
+      // Display error message to the user, e.g., in the error bar
+    });
+}
+
+// Function to fetch and display images in the gallery
+function displayImageGallery() {
+  const imageGalleryContainer = document.getElementById('image-gallery');
+
+  // Clear the container before adding images (to avoid duplicates)
+  imageGalleryContainer.innerHTML = '';
+
+  // Assuming you have already initialized Firebase and Firebase Storage
+  const storageRef = firebase.storage().ref();
+
+  // Fetch images from Firebase Storage and display them
+  storageRef.child('gallery_images').listAll()
+    .then((res) => {
+      res.items.forEach((item) => {
+        // Create a div to display each image in the gallery
+        const galleryImageDiv = document.createElement('div');
+        galleryImageDiv.classList.add('gallery-image');
+
+        // Create an img element for the image
+        const image = document.createElement('img');
+        image.src = item.getDownloadURL();
+        // Set the alt attribute if needed
+        image.alt = 'Gallery Image';
+
+        // Add Delete button to each image
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-image-btn');
+        deleteButton.textContent = 'Delete';
+        deleteButton.dataset.imageId = item.name;
+
+        // Append the image and Delete button to the gallery image div
+        galleryImageDiv.appendChild(image);
+        galleryImageDiv.appendChild(deleteButton);
+
+        // Append the gallery image div to the container
+        imageGalleryContainer.appendChild(galleryImageDiv);
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching images:', error);
+    });
+}
+
+// Event listener for submitting the Upload Image form
+document.getElementById('upload-image-form').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Get form values
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  // Get the selected file from the input
+  const imageFile = document.getElementById('image-upload').files[0];
 
-  // Call the function to login the admin user
-  loginAdminUser(email, password);
+  // Call the function to upload the image to Firebase Storage
+  uploadImageToStorage(imageFile);
 });
 
+// Event listener for dynamically added Delete buttons in the gallery
+document.getElementById('image-gallery').addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-image-btn')) {
+    // Get the image ID associated with the Delete button
+    const imageId = event.target.dataset.imageId;
+
+    // Call the function to delete the image from Firebase Storage
+    deleteImageFromStorage(imageId);
+  }
+});
+
+// Call the function to display images in the gallery on page load
+displayImageGallery();
