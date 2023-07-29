6bc018d5049e32196f864f3cc7abd72bf1c67052
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-29 17:46:48 -0500

    Update Authentication.js

--- backend/Authentication.js
+++ backend/Authentication.js
@@ -1,1 +1,28 @@
+// Authentication JavaScript (authentication.js)
+
+// Function to authenticate admin users with email and password
+function loginAdminUser(email, password) {
+  // Assuming you have already initialized Firebase and Firebase Auth
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('Admin user logged in successfully.');
+      // Redirect to the admin dashboard page or update UI as needed
+    })
+    .catch((error) => {
+      console.error('Error logging in:', error);
+      // Display error message to the user, e.g., in the error bar
+    });
+}
+
+// Event listener for submitting the Login form
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get form values
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Call the function to login the admin user
+  loginAdminUser(email, password);
+});
 
