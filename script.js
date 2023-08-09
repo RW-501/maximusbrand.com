42cf9c1e367187b1be41d979eac8aff65796ddb4
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-09 14:29:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,23 @@
-// Assuming fetchFeaturedProducts() returns an array of featured products
-function fetchFeaturedProducts() {
-  // Fetch featured products from the server or data source
-  return fetch('your-api-url/featured-products')
-    .then((response) => response.json())
-    .catch((error) => console.error('Error fetching featured products:', error));
-}
+document.addEventListener("DOMContentLoaded", () => {
+    // Sample JSON data for gallery images (replace with your actual image URLs)
+    const galleryData = [
+        "placeholder-image1.jpg",
+        "placeholder-image2.jpg",
+        "placeholder-image3.jpg",
+        // Add more image URLs here
+    ];
 
-function displayFeaturedProducts() {
-  const featuredProductsSection = document.getElementById('featured-products');
+    const imageGallery = document.getElementById("imageGallery");
 
-  // Call the fetchFeaturedProducts() function
-  fetchFeaturedProducts().then((featuredProducts) => {
-    // Create HTML elements for each featured product and append them to the section
-    featuredProducts.forEach((product) => {
-      const productElement = document.createElement('div');
-      // Customize the product element with the product data (image, description, etc.)
-      productElement.innerHTML = `<img src="${product.image}" alt="${product.name}">
-                                    <p>${product.description}</p>`;
-      featuredProductsSection.appendChild(productElement);
+    galleryData.forEach(imageUrl => {
+        const imageCard = document.createElement("div");
+        imageCard.classList.add("image-card");
+
+        const image = document.createElement("img");
+        image.src = imageUrl;
+        image.alt = "Gallery Image";
+
+        imageCard.appendChild(image);
+        imageGallery.appendChild(imageCard);
     });
-  });
-}
-
-// Call the displayFeaturedProducts() function to populate the section
-displayFeaturedProducts();
-
-
-// Assuming fetchInstagramFeed() returns an array of Instagram images
-function fetchInstagramFeed() {
-  // Fetch Instagram images from the server or API
-  return fetch('your-api-url/instagram-feed')
-    .then((response) => response.json())
-    .catch((error) => console.error('Error fetching Instagram feed:', error));
-}
-
-function displayInstagramFeed() {
-  const instagramFeedSection = document.getElementById('instagram-feed');
-
-  // Call the fetchInstagramFeed() function
-  fetchInstagramFeed().then((instagramImages) => {
-    // Create HTML elements for each Instagram image and append them to the section
-    instagramImages.forEach((image) => {
-      const imageElement = document.createElement('img');
-      imageElement.src = image.url;
-      imageElement.alt = 'Instagram Image';
-      instagramFeedSection.appendChild(imageElement);
-    });
-  });
-}
-
-// Call the displayInstagramFeed() function to populate the section
-displayInstagramFeed();
+});
