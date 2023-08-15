15f7c464f4ba941920df4bb7adb3e9ddbb609d12
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-14 21:53:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
+// Function to achieve a random-like sort using the sort() method
+function randomLikeSort(a, b) {
+    return Math.random() - 0.5; // Returns a random number between -0.5 and 0.5
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     // Sample JSON data for gallery images (replace with your actual image URLs)
-    const galleryData = [
-        "placeholder-image1.jpg",
-        "placeholder-image2.jpg",
-        "placeholder-image3.jpg",
+
+    const imageGalleryDataArray = [
+        "/gallery/1.jpg",
+        "/gallery/2.jpg",
+        "/gallery/3.jpg",
+        "/gallery/4.jpg",
+        "/gallery/5.jpg",
+        "/gallery/6.jpg",
+        "/gallery/7.jpg",
+        "/gallery/8.jpg",
+        "/gallery/9.jpg",
+        "/gallery/10.jpg",
+        "/gallery/11.jpg",
+        "/gallery/12.jpg",
+        "/gallery/13.jpg",
+        "/gallery/14.jpg",
+        "/gallery/15.jpg",
+     
         // Add more image URLs here
     ];
+     const  imageGalleryData.sort(imageGalleryDataArray);
+
+    const imageGalleryHome = document.getElementById("imageGalleryHome");
+
+    imageGalleryData.forEach(imageHomeUrl => {
+        const imageCard = document.createElement("div");
+        imageCard.classList.add("image-card");
+
+        const imageHome = document.createElement("img");
+        imageHome.src = imageHomeUrl;
+        imageHome.alt = "Gallery Image";
+
+        imageHomeCard.appendChild(imageHome);
+        imageGalleryHome.appendChild(imageHomeCard);
+    });
+
+
+
+    
+document.addEventListener("DOMContentLoaded", () => {
+    // Sample JSON data for gallery images (replace with your actual image URLs)
+    const galleryDataArray = [
+        "/gallery/16.jpg",
+        "/gallery/17.jpg",
+        "/gallery/18.jpg",
+        "/gallery/19.jpg",
+        "/gallery/20.jpg",
+        "/gallery/21.jpg",
+        "/gallery/22.jpg",
+        "/gallery/23.jpg",
+        "/gallery/24.jpg",
+        "/gallery/25.jpg",
+        "/gallery/26.jpg",
+        "/gallery/27.jpg",
+        "/gallery/28.jpg",
+        "/gallery/29.jpg",
+        "/gallery/30.jpg",
+
+    ];
+     const  imageGalleryData.sort(galleryDataArray);
 
     const imageGallery = document.getElementById("imageGallery");
 
@@ -37,7 +98,25 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         // Add more product data here
     ];
+// Custom comparison function to sort by price then name
+function compareProducts(a, b) {
+    const priceA = parseFloat(a.price.replace("$", ""));
+    const priceB = parseFloat(b.price.replace("$", ""));
 
+    // Sort by price
+    if (priceA < priceB) {
+        return -1;
+    }
+    if (priceA > priceB) {
+        return 1;
+    }
+
+    // If prices are equal, sort by name
+    return a.name.localeCompare(b.name);
+}
+
+// Sort the productsData array using the custom comparison function
+productsData.sort(compareProducts);
     const productGrid = document.getElementById("productGrid");
 
     productsData.forEach(product => {
